aab6e10e8c1830318589c83c36ae7e0ac1065cb3
Shrinivas Harapanahalli <shrinivas.harapanahalli@altair.com>
2019-11-20 22:57:31 +0530

    TC_dev created new test suite TestPbsNodeRampDownKeepSelect

---
+++ test/tests/functional/pbs_node_rampdown_keep_select.py
@@ -0,0 +1,407 @@
+# coding: utf-8
+
+# Copyright (C) 1994-2019 Altair Engineering, Inc.
+# For more information, contact Altair at www.altair.com.
+#
+# This file is part of the PBS Professional ("PBS Pro") software.
+#
+# Open Source License Information:
+#
+# PBS Pro is free software. You can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# PBS Pro is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Commercial License Information:
+#
+# For a copy of the commercial license terms and conditions,
+# go to: (http://www.pbspro.com/UserArea/agreement.html)
+# or contact the Altair Legal Department.
+#
+# Altair’s dual-license business model allows companies, individuals, and
+# organizations to create proprietary derivative works of PBS Pro and
+# distribute them - whether embedded or bundled with other software -
+# under a commercial license agreement.
+#
+# Use of Altair’s trademarks, including but not limited to "PBS™",
+# "PBS Professional®", and "PBS Pro™" and Altair’s logos is subject to Altair's
+# trademark licensing policies.
+
+from tests.functional import *
+from os import name
+from collections import Counter
+
+
+class n_conf:
+    """
+    used to define node configuration info
+    """
+    def __init__(self, node_a={}, node_ct=0, usenatvn=False):
+        self.vnode_ct = node_ct
+        self.a = node_a
+        self.usenatvnode = usenatvn
+
+
+class new_res:
+    """
+    used to define new custom resource
+    """
+    def __init__(self, res_name, res_a={}):
+        self.res_name = res_name
+        self.a = res_a
+
+
+class test_config:
+    """
+    used to store config of a test case
+    """
+    def __init__(self, qsub_sel, keep_sel, sched_sel, expected_res, job_stat,
+                 rel_user, qsub_sel_after, sched_sel_after, job_stat_after,
+                 expected_res_after):
+        self.qsub_sel = qsub_sel
+        self.keep_sel = keep_sel
+        self.sched_sel = sched_sel
+        self.expected_res = expected_res
+        self.job_stat = job_stat
+        self.rel_user = rel_user
+        self.qsub_sel_after = qsub_sel_after
+        self.sched_sel_after = sched_sel_after
+        self.job_stat_after = job_stat_after
+        self.expected_res_after = expected_res_after
+
+
+@requirements(num_moms=5)
+class TestPbsNodeRampDownKeepSelect(TestFunctional):
+    """
+    This tests the Node Rampdown Feature's extension called keep_select ,
+    where while a job is running, nodes/resources assigned on non-mother
+    superior can be released by specifying a subselect.
+
+    Custom parameters:
+    moms: colon-separated hostnames of five MoMs
+    """
+
+    res_s_h = {'type': 'string', 'flag': 'h'}
+    res_b_h = {'type': 'boolean', 'flag': 'h'}
+    res_l_nh = {'type': 'long', 'flag': 'nh'}
+
+    def pbs_nodefile_match_exec_host(self, jid, ehost, schedselect=None):
+        """
+        Look into the PBS_NODEFILE on the first host listed in 'exec_host'
+        and returns True if all host entries in 'exec_host' match the entries
+        in the file. Otherwise, return False.
+
+        # Look for 'mpiprocs' values in 'schedselect' (if not None), and
+        # verify that the corresponding node hosts are appearing in
+        # PBS_NODEFILE 'mpiprocs' number of times.
+        """
+
+        pbs_nodefile = os.path.join(self.server.
+                                    pbs_conf['PBS_HOME'], 'aux', jid)
+
+        # look for mpiprocs settings
+        mpiprocs = []
+        if schedselect is not None:
+            select_list = schedselect.split('+')
+
+            for chunk in select_list:
+                ch_ct = '1'
+                chl = chunk.split(':')
+                if chl[0].isnumeric():
+                    ch_ct = chl[0]
+                    del chl[0]
+                for x in range(int(ch_ct)):
+                    tmpmpi = '1'
+                    for ch in chl:
+                        if ch.find('=') != -1:
+                            c = ch.split('=')
+                            if c[0] == "mpiprocs":
+                                tmpmpi = c[1]
+                    mpiprocs.append(tmpmpi)
+        first_host = ehost[0]
+
+        cmd = ['cat', pbs_nodefile]
+        ret = self.server.du.run_cmd(first_host, cmd, sudo=False)
+        ehost2 = []
+        for h in ret['out']:
+            ehost2.append(h.split('.')[0])
+
+        ehost1 = []
+        for (eh, mpin) in zip(ehost, mpiprocs):
+            for k in range(int(mpin)):
+                ehost1.append(eh)
+
+        self.assertEqual(Counter(ehost1), Counter(ehost2),
+                         'PBS_NODEFILE match failed')
+
+    def match_vnode_status(self, vnode_list, state, ncpus=None, jobs=None,
+                           mem=None):
+        """
+        Given a list of vnode names in 'vnode_list', check to make
+        sure each vnode's state, jobs string, resources_assigned.mem,
+        and resources_assigned.ncpus match the passed arguments.
+        This will throw an exception if a match is not found.
+        """
+        if ncpus is None:
+            if state == 'free':
+                ncpus = [0 for x in range(len(vnode_list))]
+            else:
+                ncpus = [None for x in range(len(vnode_list))]
+        for (vn, cpus) in zip(vnode_list, ncpus):
+            dict_match = {'state': state}
+            if jobs is not None:
+                dict_match['jobs'] = jobs
+            if cpus is not None:
+                dict_match['resources_assigned.ncpus'] = cpus
+            if mem is not None:
+                dict_match['resources_assigned.mem'] = mem
+
+            self.server.expect(VNODE, dict_match, id=vn)
+
+    def create_res(self, res_list):
+        """
+        creates custom resources
+        """
+        for res in res_list:
+            self.server.manager(MGR_CMD_CREATE, RSC, res.a, id=res.res_name)
+
+    def config_nodes(self, node_conf):
+        """
+        configures nodes as per the node_conf list parameter
+        """
+        self.mom_list = []
+        self.vnode_dict = {}
+        # Now start setting up and creating the vnodes
+        self.server.manager(MGR_CMD_DELETE, NODE, None, "")
+
+        for (mom, conf) in zip(self.momArr, node_conf):
+            if mom.has_vnode_defs():
+                mom.delete_vnode_defs()
+            self.server.create_vnodes(mom.shortname, conf.a, conf.vnode_ct,
+                                      mom, delall=False,
+                                      usenatvnode=conf.usenatvnode)
+            self.vnode_dict[mom.shortname] = {'mom': mom,
+                                              'res': conf}
+            vn_ct = conf.vnode_ct
+            if conf.usenatvnode:
+                vn_ct -= 1
+            elif vn_ct:
+                self.vnode_dict[mom.shortname]['res'] = None
+            for n in range(vn_ct):
+                vnid = mom.shortname + '[' + str(n) + ']'
+                self.vnode_dict[vnid] = {'mom': mom,
+                                         'res': conf}
+            self.mom_list.append(mom)
+        self.server.expect(NODE, {'state=free': len(self.vnode_dict)})
+
+    def setUp(self):
+
+        if len(self.moms) != 5:
+            self.skip_test(reason="need 5 mom hosts: " +
+                           "-p moms=<m1>:<m2>:<m3>:<m4>:<m5>")
+
+        TestFunctional.setUp(self)
+        Job.dflt_attributes[ATTR_k] = 'oe'
+
+        self.server.cleanup_jobs()
+
+        self.momA = self.moms.values()[0]
+        self.momB = self.moms.values()[1]
+        self.momC = self.moms.values()[2]
+        self.momD = self.moms.values()[3]
+        self.momE = self.moms.values()[4]
+
+        self.momArr = [self.momA, self.momB, self.momC, self.momD, self.momE]
+
+        self.hostA = self.momA.shortname
+        self.hostB = self.momB.shortname
+        self.hostC = self.momC.shortname
+        self.hostD = self.momD.shortname
+        self.hostE = self.momE.shortname
+
+        if sys.platform in ('cygwin', 'win32'):
+            SLEEP_CMD = "pbs-sleep"
+        else:
+            SLEEP_CMD = "/bin/sleep"
+
+        self.rel_nodes_cmd = os.path.join(
+            self.server.pbs_conf['PBS_EXEC'], 'bin', 'pbs_release_nodes')
+
+    def tearDown(self):
+        self.momA.signal("-CONT")
+        self.momB.signal("-CONT")
+        self.momC.signal("-CONT")
+        self.momD.signal("-CONT")
+        self.momE.signal("-CONT")
+        TestFunctional.tearDown(self)
+        # Delete managers and operators if added
+        attrib = ['operators', 'managers']
+        self.server.manager(MGR_CMD_UNSET, SERVER, attrib)
+
+    def flatten_node_res(self, nc_list):
+        """
+        returns a list of flattened node resource dictionary
+        """
+        ret = []
+        for nc in nc_list:
+            ret.append(','.join("%s=%r" % (key, val) for key, val in
+                                sorted(nc.a.items())))
+        return ret
+
+    def get_mom_vn_execvn(self, execvnode):
+        """
+        will get vnode list and corresponding mom list from
+        the execvnode paramter
+        """
+        mlist = []
+        vlist = []
+        for chunk in execvnode:
+            if chunk.vchunk:
+                mlist.append(self.vnode_dict[chunk.vchunk[0].vnode]
+                             ['mom'].shortname)
+                for vch in chunk.vchunk:
+                    vlist.append(vch.vnode)
+            else:
+                mlist.append(self.vnode_dict[chunk.vnode]['mom'].shortname)
+                vlist.append(chunk.vnode)
+        return (mlist, vlist)
+
+    def common_tc_flow(self, tc):
+        """
+        Defines a common test case flow, the configuration of a test case is
+        passed using the 'tc' argument
+        """
+        # 1. submit the job wih select spec
+        job = Job(TEST_USER, attrs={'Resource_List.select': tc.qsub_sel})
+        job.set_sleep_time(1000)
+        jid = self.server.submit(job)
+
+        # 2. validate job state and attributes.
+        self.server.expect(JOB, tc.job_stat, id=jid)
+        js = self.server.status(JOB, [ATTR_execvnode, ATTR_exechost], jid)[0]
+
+        # 3. extract vnode list and mom list from execvnode
+        (mlist, vlist) = self.get_mom_vn_execvn(job.execvnode())
+        actual_res = self.flatten_node_res([self.vnode_dict[vn]['res'] for vn
+                                            in vlist])
+
+        # 4. compare actual resources allocated vs expected
+        self.assertEqual(Counter(tc.expected_res), Counter(actual_res),
+                         'Actual Vnode resources are not as expected')
+
+        # 5. validate hostnames in exechost correspond to vnodes in execvnode
+        self.assertEqual(Counter(mlist),
+                         Counter([list(x.keys())[0] for x in
+                                  job.exechost().hosts]),
+                         'exechost failed to correspond with execvnode')
+
+        # 6. check assigned vnodes are in job-busy state
+        self.match_vnode_status(vlist, 'job-busy',
+                                [self.vnode_dict[x]['res'].a[
+                                 'resources_available.ncpus'] for x in vlist])
+
+        # 7. validate PBS_NODEFILE
+        self.pbs_nodefile_match_exec_host(jid, mlist, tc.sched_sel)
+
+        # 8. (drum rolls) submit release node command
+        cmd = [self.rel_nodes_cmd, '-j', jid, '-k', tc.keep_sel]
+        ret = self.server.du.run_cmd(self.server.hostname, cmd,
+                                     runas=tc.rel_user)
+        self.assertEqual(ret['rc'], 0)
+
+        # 9. verify job state and attributes are as expected
+        self.server.expect(JOB, tc.job_stat_after, id=jid)
+
+        js = self.server.status(JOB, [ATTR_execvnode, ATTR_exechost], jid)[0]
+
+        # 10. extract vnode list and mom list from execvnode
+        (mlist_new, vlist_new) = self.get_mom_vn_execvn(job.execvnode())
+        actual_res_after = self.flatten_node_res([self.vnode_dict[vn]['res']
+                                                  for vn in vlist_new])
+
+        # 11. compare actual resources allocated vs expected
+        self.assertEqual(Counter(tc.expected_res_after),
+                         Counter(actual_res_after),
+                         'Actual Vnode resources are not as expected')
+
+        # 12. validate hostnames in exechost correspond to vnodes in execvnode
+        self.assertEqual(Counter(mlist_new),
+                         Counter([list(x.keys())[0] for x in
+                                 job.exechost().hosts]),
+                         'exechost failed to correspond with execvnode')
+
+        # 13. check assigned vnodes are in job-busy state
+        self.match_vnode_status(vlist_new, 'job-busy',
+                                [self.vnode_dict[x]['res'].a[
+                                 'resources_available.ncpus'] for x in
+                                 vlist_new])
+
+        # 14. compute freed vnodes resource list
+        freed_res = list(set(vlist) - set(vlist_new))
+
+        # 15. check freed vnodes are in 'free' state
+        self.match_vnode_status(freed_res, 'free')
+
+        # 16. validate PBS_NODEFILE again
+        self.pbs_nodefile_match_exec_host(jid, mlist_new, tc.sched_sel_after)
+
+    def test_basic_use_case_ncpus(self, rel_user=TEST_USER):
+        """
+        submit job with below select string
+        'select=ncpus=1+2:ncpus=2+2:ncpus=3:mpiprocs=2'
+        release nodes except the MS and nodes matching below sub select string
+        'select=ncpus=2+ncpus=3:mpiprocs=2'
+        """
+        n1 = n_conf({'resources_available.ncpus': '1'})
+        n2 = n_conf({'resources_available.ncpus': '2'})
+        n3 = n_conf({'resources_available.ncpus': '3'})
+
+        nc_list = [n1, n2, n2, n3, n3]
+        # 1. configure the cluster
+        self.config_nodes(nc_list)
+
+        args = {
+            'qsub_sel': 'ncpus=1+2:ncpus=2+2:ncpus=3:mpiprocs=2',
+            'keep_sel': 'select=ncpus=2+ncpus=3:mpiprocs=2',
+            'sched_sel': '1:ncpus=1+2:ncpus=2+2:ncpus=3:mpiprocs=2',
+            'expected_res': self.flatten_node_res(nc_list),
+            'rel_user': rel_user,
+            'qsub_sel_after': '1:ncpus=1+1:ncpus=2+1:ncpus=3:mpiprocs=2',
+            'sched_sel_after': '1:ncpus=1+1:ncpus=2+1:ncpus=3:mpiprocs=2',
+            'expected_res_after': self.flatten_node_res([n1, n2, n3])
+            }
+
+        job_stat = {'job_state': 'R',
+                    'Resource_List.ncpus': 11,
+                    'Resource_List.nodect': 5,
+                    'Resource_List.select': args['qsub_sel'],
+                    'schedselect': args['sched_sel']}
+
+        args['job_stat'] = job_stat
+
+        job_stat_after = {'job_state': 'R',
+                          'Resource_List.ncpus': 6,
+                          'Resource_List.nodect': 3,
+                          'Resource_List.select': args['qsub_sel_after'],
+                          'schedselect': args['sched_sel_after']}
+
+        args['job_stat_after'] = job_stat_after
+        tc = test_config(**args)
+        self.common_tc_flow(tc)
+
+    def test_basic_use_case_ncpus_as_root(self):
+        """
+        submit job with below select string
+        'select=ncpus=1+2:ncpus=2+2:ncpus=3:mpiprocs=2'
+        as root release nodes except the MS and nodes matching below sub
+        select string 'select=ncpus=2+ncpus=3:mpiprocs=2'
+        """
+        self.test_basic_use_case_ncpus(rel_user=ROOT_USER)
